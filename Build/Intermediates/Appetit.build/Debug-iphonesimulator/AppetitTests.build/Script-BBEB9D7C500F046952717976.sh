d9153a1f0cabc1447e5faab130a341f0f38c23d2
douglastaquary <douglasalexandre@infoway-pi.com.br>
2017-01-24 17:23:23 -0300

    Adicionado screenshots do novo layout

---
+++ Build/Intermediates/Appetit.build/Debug-iphonesimulator/AppetitTests.build/Script-BBEB9D7C500F046952717976.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+"${SRCROOT}/Pods/Target Support Files/Pods-AppetitTests/Pods-AppetitTests-frameworks.sh"
+
---
+++ Build/Intermediates/Pods.build/Debug-iphonesimulator/Fakery.build/DerivedSources/Fakery_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char FakeryVersionString[];
+ extern const double FakeryVersionNumber;
+
+ const unsigned char FakeryVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Fakery  PROJECT:Pods-1" "\n";
+ const double FakeryVersionNumber __attribute__ ((used)) = (double)1.;
